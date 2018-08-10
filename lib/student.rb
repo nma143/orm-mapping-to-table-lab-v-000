9beb83205084b2ba1f8535b1a90f37a6c569dd32
Nikki A <nikki.apostolakis@gmail.com>
2018-08-10 19:13:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,4 +30,14 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+    DB[:conn].execute(sql, @name, @grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+
+  end
+
 end
