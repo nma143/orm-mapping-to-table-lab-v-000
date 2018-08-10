e9b67549d9790184ae0aec878e6ce23708a41283
Nikki A <nikki.apostolakis@gmail.com>
2018-08-10 19:09:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,16 @@ class Student
     @id = id
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS studnts (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
 
 
 
